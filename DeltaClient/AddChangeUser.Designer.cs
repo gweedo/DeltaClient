e0c32fdcdf33f2334383aba9bc385f31544dfd6e
sonc <guido.s1998@gmail.com>
2019-07-25 16:33:50 +0200

    Accidenti

---
+++ DeltaClient/AddChangeUser.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace DeltaClient
+{
+    partial class AddChangeUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.UserLabel = new System.Windows.Forms.Label();
+            this.NameUserLabel = new System.Windows.Forms.Label();
+            this.LicenseUserLabel = new System.Windows.Forms.Label();
+            this.PasswordUserLabel = new System.Windows.Forms.Label();
+            this.LicensePointsUserLabel = new System.Windows.Forms.Label();
+            this.EmailUserLabel = new System.Windows.Forms.Label();
+            this.LicenseExpirationUserLabel = new System.Windows.Forms.Label();
+            this.ConfirmPasswordLabel = new System.Windows.Forms.Label();
+            this.NameUsertextBox = new System.Windows.Forms.TextBox();
+            this.PasswordUserTextBox = new System.Windows.Forms.TextBox();
+            this.EmailUserTextBox = new System.Windows.Forms.TextBox();
+            this.LicensePointsUserTextBox = new System.Windows.Forms.TextBox();
+            this.LicenseExpirationUserTextBox = new System.Windows.Forms.TextBox();
+            this.ConfirmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LicenseUserTextBox = new System.Windows.Forms.TextBox();
+            this.SaveUserButton = new System.Windows.Forms.Button();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel4,
+            this.toolStripSeparator1,
+            this.toolStripLabel3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(118, 478);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(115, 41);
+            this.toolStripLabel1.Text = "Utenti";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(115, 41);
+            this.toolStripLabel2.Text = "Veicolo";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(115, 6);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(115, 41);
+            this.toolStripLabel3.Text = "Esci";
+            // 
+            // UserLabel
+            // 
+            this.UserLabel.AutoSize = true;
+            this.UserLabel.Font = new System.Drawing.Font("Segoe UI Light", 18F);
+            this.UserLabel.Location = new System.Drawing.Point(431, 36);
+            this.UserLabel.Name = "UserLabel";
+            this.UserLabel.Size = new System.Drawing.Size(88, 41);
+            this.UserLabel.TabIndex = 5;
+            this.UserLabel.Text = "label1";
+            // 
+            // NameUserLabel
+            // 
+            this.NameUserLabel.AutoSize = true;
+            this.NameUserLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.NameUserLabel.Location = new System.Drawing.Point(161, 98);
+            this.NameUserLabel.Name = "NameUserLabel";
+            this.NameUserLabel.Size = new System.Drawing.Size(77, 32);
+            this.NameUserLabel.TabIndex = 6;
+            this.NameUserLabel.Text = "Nome";
+            // 
+            // LicenseUserLabel
+            // 
+            this.LicenseUserLabel.AutoSize = true;
+            this.LicenseUserLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.LicenseUserLabel.Location = new System.Drawing.Point(161, 217);
+            this.LicenseUserLabel.Name = "LicenseUserLabel";
+            this.LicenseUserLabel.Size = new System.Drawing.Size(90, 32);
+            this.LicenseUserLabel.TabIndex = 7;
+            this.LicenseUserLabel.Text = "Patente";
+            this.LicenseUserLabel.Click += new System.EventHandler(this.LicenseUserLabel_Click);
+            // 
+            // PasswordUserLabel
+            // 
+            this.PasswordUserLabel.AutoSize = true;
+            this.PasswordUserLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.PasswordUserLabel.Location = new System.Drawing.Point(161, 323);
+            this.PasswordUserLabel.Name = "PasswordUserLabel";
+            this.PasswordUserLabel.Size = new System.Drawing.Size(107, 32);
+            this.PasswordUserLabel.TabIndex = 8;
+            this.PasswordUserLabel.Text = "Password";
+            // 
+            // LicensePointsUserLabel
+            // 
+            this.LicensePointsUserLabel.AutoSize = true;
+            this.LicensePointsUserLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.LicensePointsUserLabel.Location = new System.Drawing.Point(505, 217);
+            this.LicensePointsUserLabel.Name = "LicensePointsUserLabel";
+            this.LicensePointsUserLabel.Size = new System.Drawing.Size(66, 32);
+            this.LicensePointsUserLabel.TabIndex = 9;
+            this.LicensePointsUserLabel.Text = "Punti";
+            // 
+            // EmailUserLabel
+            // 
+            this.EmailUserLabel.AutoSize = true;
+            this.EmailUserLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.EmailUserLabel.Location = new System.Drawing.Point(502, 108);
+            this.EmailUserLabel.Name = "EmailUserLabel";
+            this.EmailUserLabel.Size = new System.Drawing.Size(69, 32);
+            this.EmailUserLabel.TabIndex = 10;
+            this.EmailUserLabel.Text = "Email";
+            // 
+            // LicenseExpirationUserLabel
+            // 
+            this.LicenseExpirationUserLabel.AutoSize = true;
+            this.LicenseExpirationUserLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.LicenseExpirationUserLabel.Location = new System.Drawing.Point(668, 217);
+            this.LicenseExpirationUserLabel.Name = "LicenseExpirationUserLabel";
+            this.LicenseExpirationUserLabel.Size = new System.Drawing.Size(111, 32);
+            this.LicenseExpirationUserLabel.TabIndex = 11;
+            this.LicenseExpirationUserLabel.Text = "Scadenza";
+            // 
+            // ConfirmPasswordLabel
+            // 
+            this.ConfirmPasswordLabel.AutoSize = true;
+            this.ConfirmPasswordLabel.Font = new System.Drawing.Font("Segoe UI Light", 14F);
+            this.ConfirmPasswordLabel.Location = new System.Drawing.Point(502, 323);
+            this.ConfirmPasswordLabel.Name = "ConfirmPasswordLabel";
+            this.ConfirmPasswordLabel.Size = new System.Drawing.Size(214, 32);
+            this.ConfirmPasswordLabel.TabIndex = 12;
+            this.ConfirmPasswordLabel.Text = "Conferma Password";
+            // 
+            // NameUsertextBox
+            // 
+            this.NameUsertextBox.Font = new System.Drawing.Font("Segoe UI Light", 11F);
+            this.NameUsertextBox.Location = new System.Drawing.Point(167, 143);
+            this.NameUsertextBox.Name = "NameUsertextBox";
+            this.NameUsertextBox.Size = new System.Drawing.Size(233, 32);
+            this.NameUsertextBox.TabIndex = 13;
+            // 
+            // PasswordUserTextBox
+            // 
+            this.PasswordUserTextBox.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.PasswordUserTextBox.Location = new System.Drawing.Point(167, 358);
+            this.PasswordUserTextBox.Name = "PasswordUserTextBox";
+            this.PasswordUserTextBox.Size = new System.Drawing.Size(233, 34);
+            this.PasswordUserTextBox.TabIndex = 14;
+            // 
+            // EmailUserTextBox
+            // 
+            this.EmailUserTextBox.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.EmailUserTextBox.Location = new System.Drawing.Point(508, 143);
+            this.EmailUserTextBox.Name = "EmailUserTextBox";
+            this.EmailUserTextBox.Size = new System.Drawing.Size(283, 34);
+            this.EmailUserTextBox.TabIndex = 15;
+            // 
+            // LicensePointsUserTextBox
+            // 
+            this.LicensePointsUserTextBox.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.LicensePointsUserTextBox.Location = new System.Drawing.Point(508, 252);
+            this.LicensePointsUserTextBox.Name = "LicensePointsUserTextBox";
+            this.LicensePointsUserTextBox.Size = new System.Drawing.Size(125, 34);
+            this.LicensePointsUserTextBox.TabIndex = 16;
+            // 
+            // LicenseExpirationUserTextBox
+            // 
+            this.LicenseExpirationUserTextBox.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.LicenseExpirationUserTextBox.Location = new System.Drawing.Point(679, 252);
+            this.LicenseExpirationUserTextBox.Name = "LicenseExpirationUserTextBox";
+            this.LicenseExpirationUserTextBox.Size = new System.Drawing.Size(125, 34);
+            this.LicenseExpirationUserTextBox.TabIndex = 17;
+            // 
+            // ConfirmPasswordTextBox
+            // 
+            this.ConfirmPasswordTextBox.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.ConfirmPasswordTextBox.Location = new System.Drawing.Point(508, 358);
+            this.ConfirmPasswordTextBox.Name = "ConfirmPasswordTextBox";
+            this.ConfirmPasswordTextBox.Size = new System.Drawing.Size(283, 34);
+            this.ConfirmPasswordTextBox.TabIndex = 18;
+            // 
+            // LicenseUserTextBox
+            // 
+            this.LicenseUserTextBox.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.LicenseUserTextBox.Location = new System.Drawing.Point(167, 252);
+            this.LicenseUserTextBox.Name = "LicenseUserTextBox";
+            this.LicenseUserTextBox.Size = new System.Drawing.Size(233, 34);
+            this.LicenseUserTextBox.TabIndex = 19;
+            // 
+            // SaveUserButton
+            // 
+            this.SaveUserButton.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.SaveUserButton.Location = new System.Drawing.Point(706, 418);
+            this.SaveUserButton.Name = "SaveUserButton";
+            this.SaveUserButton.Size = new System.Drawing.Size(85, 37);
+            this.SaveUserButton.TabIndex = 20;
+            this.SaveUserButton.Text = "Salva";
+            this.SaveUserButton.UseVisualStyleBackColor = true;
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(115, 41);
+            this.toolStripLabel4.Text = "Segnala";
+            // 
+            // AddChangeUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 478);
+            this.Controls.Add(this.SaveUserButton);
+            this.Controls.Add(this.LicenseUserTextBox);
+            this.Controls.Add(this.ConfirmPasswordTextBox);
+            this.Controls.Add(this.LicenseExpirationUserTextBox);
+            this.Controls.Add(this.LicensePointsUserTextBox);
+            this.Controls.Add(this.EmailUserTextBox);
+            this.Controls.Add(this.PasswordUserTextBox);
+            this.Controls.Add(this.NameUsertextBox);
+            this.Controls.Add(this.ConfirmPasswordLabel);
+            this.Controls.Add(this.LicenseExpirationUserLabel);
+            this.Controls.Add(this.EmailUserLabel);
+            this.Controls.Add(this.LicensePointsUserLabel);
+            this.Controls.Add(this.PasswordUserLabel);
+            this.Controls.Add(this.LicenseUserLabel);
+            this.Controls.Add(this.NameUserLabel);
+            this.Controls.Add(this.UserLabel);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "AddChangeUser";
+            this.Text = "AddChangeUser";
+            this.Load += new System.EventHandler(this.AddChangeUser_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.Label UserLabel;
+        private System.Windows.Forms.Label NameUserLabel;
+        private System.Windows.Forms.Label LicenseUserLabel;
+        private System.Windows.Forms.Label PasswordUserLabel;
+        private System.Windows.Forms.Label LicensePointsUserLabel;
+        private System.Windows.Forms.Label EmailUserLabel;
+        private System.Windows.Forms.Label LicenseExpirationUserLabel;
+        private System.Windows.Forms.Label ConfirmPasswordLabel;
+        private System.Windows.Forms.TextBox NameUsertextBox;
+        private System.Windows.Forms.TextBox PasswordUserTextBox;
+        private System.Windows.Forms.TextBox EmailUserTextBox;
+        private System.Windows.Forms.TextBox LicensePointsUserTextBox;
+        private System.Windows.Forms.TextBox LicenseExpirationUserTextBox;
+        private System.Windows.Forms.TextBox ConfirmPasswordTextBox;
+        private System.Windows.Forms.TextBox LicenseUserTextBox;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.Button SaveUserButton;
+    }
+}
