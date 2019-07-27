60aff9d0f06d6a5da7f2b7cae685bf29b5cca69e
sonc <guido.s1998@gmail.com>
2019-07-27 09:57:30 +0200

    aggiunta funzione al bottone in carDataUser

--- DeltaClient/CarDataForm.cs
+++ DeltaClient/CarDataForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DeltaClient.Car;
 
 namespace DeltaClient
 {
@@ -19,7 +20,10 @@ namespace DeltaClient
         protected int Kilometers;
         protected int BurnedLiters;
 
-        public CarDataForm(string plateNumber, string make, string model, int year, int kilometers, int burnedLiters)
+        protected string Email;
+        protected string MD5Password;
+
+        public CarDataForm(string plateNumber, string make, string model, int year, int kilometers, int burnedLiters, string email, string md5Password)
         {
             PlateNumber = plateNumber;
             PlateNumberCarTextBox.Text = plateNumber;
@@ -38,10 +42,31 @@ namespace DeltaClient
 
             BurnedLiters = burnedLiters;
             burnedLitersCarNumbers.Value = burnedLiters;
+
+            Email = email;
+            MD5Password = md5Password;
+
             InitializeComponent();
         }
 
+        private void OnClickSendButton()
+        {
+            if (PlateNumber != PlateNumberCarTextBox.Text || Make != MakeCarTextBox.Text || Model != ModelCarTextBox.Text || Year != YearCarNumbers.Value || Kilometers != KilometersCarNumbers.Value || BurnedLiters != burnedLitersCarNumbers.Value)
+            {
+                CarManagerClient carManager = new CarManagerClient();
+                Car.Car car = new Car.Car();
+                car.PlateNumber = PlateNumberCarTextBox.Text;
+                car.Make = MakeCarTextBox.Text;
+                car.Model = ModelCarTextBox.Text;
+                car.Year = Convert.ToInt16(YearCarNumbers.Value);
+                car.Kilometers = Convert.ToInt16(KilometersCarNumbers.Value);
+                car.BurnedLiters = Convert.ToInt16(burnedLitersCarNumbers.Value);
+                if (carManager.UpdateCar(car, Email, MD5Password)) MessageBox.Show("operazione eseguita con successo");
+                else MessageBox.Show("aiai e mo che se fa?");
+                   
+            }
 
+        }
 
     }
 }
