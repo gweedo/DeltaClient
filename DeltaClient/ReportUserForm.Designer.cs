def1761c6136d4c1c8283a1386e74d0f500ef027
sonc <guido.s1998@gmail.com>
2019-07-27 18:27:12 +0200

    creazione della report form

---
+++ DeltaClient/ReportUserForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace DeltaClient
+{
+    partial class ReportUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BookingListComboBox = new System.Windows.Forms.ComboBox();
+            this.SubjectTextBox = new System.Windows.Forms.TextBox();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.InviaButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BookingListComboBox
+            // 
+            this.BookingListComboBox.FormattingEnabled = true;
+            this.BookingListComboBox.Location = new System.Drawing.Point(97, 91);
+            this.BookingListComboBox.Name = "BookingListComboBox";
+            this.BookingListComboBox.Size = new System.Drawing.Size(495, 25);
+            this.BookingListComboBox.TabIndex = 0;
+            // 
+            // SubjectTextBox
+            // 
+            this.SubjectTextBox.Location = new System.Drawing.Point(62, 197);
+            this.SubjectTextBox.Name = "SubjectTextBox";
+            this.SubjectTextBox.Size = new System.Drawing.Size(209, 25);
+            this.SubjectTextBox.TabIndex = 1;
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(372, 197);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(236, 157);
+            this.MessageTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Segnalazioni";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Light", 11F);
+            this.label2.Location = new System.Drawing.Point(268, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prenotazione";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Light", 11F);
+            this.label3.Location = new System.Drawing.Point(57, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Oggetto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Light", 11F);
+            this.label4.Location = new System.Drawing.Point(367, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Messaggio";
+            // 
+            // InviaButton
+            // 
+            this.InviaButton.Font = new System.Drawing.Font("Segoe UI Light", 10F);
+            this.InviaButton.Location = new System.Drawing.Point(533, 389);
+            this.InviaButton.Name = "InviaButton";
+            this.InviaButton.Size = new System.Drawing.Size(75, 29);
+            this.InviaButton.TabIndex = 7;
+            this.InviaButton.Text = "Invia";
+            this.InviaButton.UseVisualStyleBackColor = true;
+            this.InviaButton.Click += new System.EventHandler(this.InviaButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Light", 10F);
+            this.button2.Location = new System.Drawing.Point(402, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Annulla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.cancelReport);
+            // 
+            // ReportUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 478);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.InviaButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.SubjectTextBox);
+            this.Controls.Add(this.BookingListComboBox);
+            this.Font = new System.Drawing.Font("Segoe UI Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "ReportUserForm";
+            this.Text = "ReportUserForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox BookingListComboBox;
+        private System.Windows.Forms.TextBox SubjectTextBox;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button InviaButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ DeltaClient/ReportUserForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DeltaClient.Booking;
+using DeltaClient.Report;
+
+namespace DeltaClient
+{
+    public partial class ReportUserForm : Form
+    {
+        protected string Email;
+        protected string PasswordHash;
+        private BookingManagerClient bookingManager;
+        private Booking.Booking[] bookings;
+
+        public ReportUserForm(string email, string passwordHash)
+        {
+            Email = email;
+            PasswordHash = passwordHash;
+            InitializeComponent();
+
+            this.bookingManager = new BookingManagerClient();
+            bookings = bookingManager.GetBookings(Email, PasswordHash);
+            foreach (Booking.Booking SingleBooking in bookings) {
+                BookingListComboBox.Items.Add(SingleBooking);
+            }
+        }
+
+        private void InviaButton_Click(object sender, EventArgs e)
+        {
+            if (SubjectTextBox.Text == ("") || MessageTextBox.Text == ("")) MessageBox.Show("riempire i campi di testo");
+            else if (BookingListComboBox.SelectedIndex == -1) MessageBox.Show("selezionare una prenotazione");//la combobox restituisce -1 se non è stato selezionato nessun elemento
+            else
+            {
+                Booking.Booking Booked = bookings[BookingListComboBox.SelectedIndex];
+                Report.Booking selectedBooking = new Report.Booking();
+
+                selectedBooking.ID = Booked.ID;
+                selectedBooking.Start = Booked.Start;
+                selectedBooking.End = Booked.End;
+                selectedBooking.Returned = Booked.Returned;
+
+                //aggiungo i dati dell'utente
+                Report.User UserReported = new Report.User();
+                UserReported.Email = Booked.Booker.Email;
+                UserReported.PasswordHash = Booked.Booker.PasswordHash;
+                UserReported.Name = Booked.Booker.Name;
+                UserReported.License = Booked.Booker.License;
+                UserReported.LicensePoints = Booked.Booker.LicensePoints;
+                UserReported.LicenseExpiration = Booked.Booker.LicenseExpiration;
+
+                selectedBooking.Booker = UserReported;
+
+                //aggiungo i dati della macchina noleggiata
+                Report.Car CarReported = new Report.Car();
+                CarReported.PlateNumber = Booked.BookedCar.PlateNumber;
+                CarReported.Make = Booked.BookedCar.Make;
+                CarReported.Model = Booked.BookedCar.Model;
+                CarReported.Year = Booked.BookedCar.Year;
+                CarReported.Kilometers = Booked.BookedCar.Kilometers;
+                CarReported.BurnedLiters = Booked.BookedCar.BurnedLiters;
+
+                selectedBooking.BookedCar = CarReported;
+
+
+                ReportManagerClient reportManager = new ReportManagerClient();
+                reportManager.AddReport(SubjectTextBox.Text, MessageTextBox.Text, selectedBooking , Email, PasswordHash);
+            }
+        }
+
+        private void cancelReport(object sender, EventArgs e)
+        {
+            UserListForm listFormChild = new UserListForm(Email, PasswordHash);
+            listFormChild.MdiParent = ParentForm;
+            listFormChild.FormBorderStyle = FormBorderStyle.None;
+            listFormChild.Dock = DockStyle.Fill;
+            listFormChild.Show();
+        }
+    }
+}
